03179016c56932bee2dec440c1d32049694b36f7
Charles Koutcheme <charles.koutcheme@gmail.com>
2019-07-09 16:41:36 +0200

    general structure of the page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
+import Header from "./Header";
+import MainContent from "./MainContent";
+import Footer from "./Footer";
 import "./App.css";
-import { Button } from "semantic-ui-react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Our project is going to be awsome</p>
-      </header>
-      <div>
-        <Button content="Primary" primary />
-        <Button content="Secondary" secondary />
-      </div>
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
---
+++ src/LeftPanel.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button } from "semantic-ui-react";
+
+function LeftPanel() {
+  return <div className="Left-panel" />;
+}
+
+export default LeftPanel;
