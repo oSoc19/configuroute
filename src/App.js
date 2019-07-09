d152ad71ec9dbb91b4bacbaf37eb41d0b5d468fb
Charles Koutcheme <charles.koutcheme@gmail.com>
2019-07-09 09:42:34 +0200

    Semantic ui framework
    Included semantic ui framework, our project should be completly set-up.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "semantic-ui-react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Hello, world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Our project is going to be awsome</p>
       </header>
+      <div>
+        <Button content="Primary" primary />
+        <Button content="Secondary" secondary />
+      </div>
     </div>
   );
 }
