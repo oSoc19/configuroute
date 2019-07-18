69869e9d382ccd52cfd2306d04a71767e4771f1c
Wouter Vandendriessche <wouter.vd3@telenet.be>
2019-07-18 16:50:17 +0200

    Updates

---
+++ precache-manifest.56ed2f94d7ff52eb9c3586584c3abbf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba71fb6496695cfd0bcfdbc6d737ae5",
+    "url": "/configuroute/index.html"
+  },
+  {
+    "revision": "389f842e2dbd29c6f897",
+    "url": "/configuroute/static/css/2.6639d161.chunk.css"
+  },
+  {
+    "revision": "523d3343180b49c06d7e",
+    "url": "/configuroute/static/css/main.22499656.chunk.css"
+  },
+  {
+    "revision": "389f842e2dbd29c6f897",
+    "url": "/configuroute/static/js/2.57a41ddf.chunk.js"
+  },
+  {
+    "revision": "523d3343180b49c06d7e",
+    "url": "/configuroute/static/js/main.741787b9.chunk.js"
+  },
+  {
+    "revision": "e1d580cf93e431b2567c",
+    "url": "/configuroute/static/js/runtime~main.250062d1.js"
+  },
+  {
+    "revision": "47bb72e506eba473225b7437ac7bdebf",
+    "url": "/configuroute/static/media/logo.47bb72e5.jpg"
+  }
+]);
